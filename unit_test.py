5dffd5f0db45af9e52db6e36faa5b67b0042951d
cesar larragueta <cesarlarra33@gmail.com>
2025-10-02 11:04:35 +0200

    ajout des test unitaires

---
+++ unit_test.py
@@ -0,0 +1,74 @@
+import numpy as np
+import cv2 as cv
+import time
+import pypot.dynamixel
+import sys
+import tty
+import termios
+import math
+from camera import detect_lines_by_color
+from camera import detect_lines_brown
+from camera import Color
+from kynematic import *
+from time import sleep
+
+
+def test_inversekinematics(): 
+    try: 
+        # --- initialisation Dynamixel ---
+        ports = pypot.dynamixel.get_available_ports()
+        if not ports:
+            exit('No Dynamixel port found')
+        dxl_io = pypot.dynamixel.DxlIO(ports[0])
+        dxl_io.set_wheel_mode([1, 2])   # active wheel mode sur les deux
+        
+        
+        
+        Vd, Vg = inverse_kinematics(0, 0.8)
+        speed_d = rad_s_to_dxl_speed(Vd)
+        speed_g = rad_s_to_dxl_speed(Vg)
+        
+
+        while True: 
+            # appliquer aux moteurs
+                dxl_io.set_moving_speed({
+                    1: -speed_d,  # moteur gauche
+                    2:  speed_g  # moteur droit
+                })
+            
+    finally : 
+        dxl_io.set_moving_speed({1: 0, 2: 0})
+        
+
+
+def test_directkinematics(): 
+    try: 
+        # --- initialisation Dynamixel ---
+        ports = pypot.dynamixel.get_available_ports()
+        if not ports:
+            exit('No Dynamixel port found')
+        dxl_io = pypot.dynamixel.DxlIO(ports[0])
+        dxl_io.set_wheel_mode([1, 2])   # active wheel mode sur les deux
+        
+        
+        v, w = direct_kinematics(-6, 6)
+        Vd, Vg = inverse_kinematics(v, w)
+        speed_d = rad_s_to_dxl_speed(Vd)
+        speed_g = rad_s_to_dxl_speed(Vg)
+        
+
+        while True: 
+            # appliquer aux moteurs
+                dxl_io.set_moving_speed({
+                    1: -speed_d,  # moteur gauche
+                    2:  speed_g  # moteur droit
+                })
+            
+    finally : 
+        dxl_io.set_moving_speed({1: 0, 2: 0})
+        
+
+
+
+
+test_directkinematics()
